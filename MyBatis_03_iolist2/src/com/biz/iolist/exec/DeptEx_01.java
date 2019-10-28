ef4e1ebb9f2077b6c1f57e38698e8e76e081859e
jewon513 <jewon513@gmail.com>
2019-10-28 16:39:27 +0900

    2019-10-28

---
+++ MyBatis_03_iolist2/src/com/biz/iolist/exec/DeptEx_01.java
@@ -0,0 +1,16 @@
+package com.biz.iolist.exec;
+
+import com.biz.iolist.service.dept.DeptServiceV1;
+
+public class DeptEx_01 {
+
+	public static void main(String[] args) {
+
+		DeptServiceV1 ds = new DeptServiceV1();
+		
+		ds.viewAllList();
+		ds.viewNameAndCEOList();
+		
+	}
+
+}
---
+++ MyBatis_03_iolist2/src/com/biz/iolist/exec/IolistEx_02.java
@@ -0,0 +1,20 @@
+package com.biz.iolist.exec;
+
+import com.biz.iolist.service.pro.ProductServiceV1;
+
+public class IolistEx_02 {
+
+	public static void main(String[] args) {
+
+		ProductServiceV1 ps = new ProductServiceV1();
+		
+		
+		ps.proUpdate();
+		
+		
+		
+		
+		
+	}
+
+}
